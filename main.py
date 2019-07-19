edabf3ac27fc4a6d82c5d5691c1c2ed6e87501b2
Justin Arieltan <agriad1@yahoo.com>
2019-07-19 14:09:19 +0200

    5 can establish connection

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import praw
+from data import *
 
 
 def main():
-    print("hello")
+    reddit = init_connect()
+    print(reddit)
+    print(reddit.read_only)
+    print(reddit.user.me())
+    print(praw.models.User.subreddits)
 
 
-def test():
-    reddit = praw.Reddit()
+def init_connect():
+    reddit = praw.Reddit(client_id=CLIENT_ID,
+                     client_secret=SECRET_KEY, password=PASSWORD,
+                     user_agent=USER_AGENT, username=USERNAME)
+    return reddit
 
 
 main()
